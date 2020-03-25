bffce39f4c5feea5f48c23435ae5a72e0bd75c7f
zhaoguo.ma <zhaoguo.ma@arcserve.com>
2020-03-25 21:57:45 +0800

    testng/extentreport

--- src/main/java/com/zhaoguom/employee/EmployeeApplication.java
+++ src/main/java/com/zhaoguom/employee/EmployeeApplication.java
@@ -2,11 +2,14 @@ package com.zhaoguom.employee;
 
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication(scanBasePackages = {"com.zhaoguom.employee"})
 @MapperScan("com.zhaoguom.employee.dao")
+@EnableAutoConfiguration(exclude={MongoAutoConfiguration.class})
 public class EmployeeApplication {
 
 	public static void main(String[] args) {
